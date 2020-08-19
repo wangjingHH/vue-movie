3990eeeae74e68785f0f85d1be9eaabde45f22a1
wangjing <wangjing.0324@qq.com>
2020-08-19 14:03:24 +0800

    add setData

--- src/components/Search/index.vue
+++ src/components/Search/index.vue
@@ -9,22 +9,13 @@
     <div class="search_result">
       <h3>电影/电视剧/综艺</h3>
       <ul>
-        <li>
-          <div class="img"><img src="/images/movie_1.jpg"></div>
+        <li v-for="item in moviesList" :key="item.id">
+          <div class="img"><img :src="item.img | setWH('128.180')"></div>
           <div class="info">
-            <p><span>无名之辈</span><span>8.5</span></p>
-            <p>A Cool Fish</p>
-            <p>剧情,喜剧,犯罪</p>
-            <p>2018-11-16</p>
-          </div>
-        </li>
-        <li>
-          <div class="img"><img src="/images/movie_1.jpg"></div>
-          <div class="info">
-            <p><span>无名之辈</span><span>8.5</span></p>
-            <p>A Cool Fish</p>
-            <p>剧情,喜剧,犯罪</p>
-            <p>2018-11-16</p>
+            <p><span>{{item.nm}}</span><span>{{item.sc}}</span></p>
+            <p>{{item.enm}}</p>
+            <p>{{item.cat}}</p>
+            <p>{{item.rt}}</p>
           </div>
         </li>
       </ul>
@@ -34,7 +25,21 @@
 
 <script>
 export default {
-  name : 'Search'
+  name : 'Search',
+  data(){
+    return {
+      moviesList : [],
+    }
+  },
+  mounted(){
+    this.axios.get("/searchList.json").then((res)=>{
+      var msg = res.data.msg;
+      var movies = res.data.data.movies;
+      if (msg && movies){
+        this.moviesList = res.data.data.movies.list;
+      }
+    })
+  }
 }
 </script>
 
